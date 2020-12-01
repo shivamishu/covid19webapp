9d68b7db3a2ea0ca529dca241ca416d8d50b14ab
nayak-codebase <praveen.nayak86@gmail.com>
2020-12-01 16:29:26 +0530

    Update dbConnection.js
    dbchanges

--- routes/dbConnection.js
+++ routes/dbConnection.js
@@ -1,20 +1,12 @@
 "use strict";
 var mysql = require("mysql");
-// var connection = mysql.createConnection({
-//   host: process.env.AWS_RDS_ENDPOINT,
-//   user: process.env.AWS_RDS_USERNAME,
-//   password: process.env.AWS_RDS_PASSWORD,
-//   port: process.env.AWS_RDS_PORT,
-//   database: process.env.AWS_RDS_DATABASE,
-//   multipleStatements: true,
-// });
-var pool  = mysql.createPool({
-  connectionLimit : 10,
-  host            : process.env.AWS_RDS_ENDPOINT,
-  user: process.env.AWS_RDS_USERNAME,
-  password: process.env.AWS_RDS_PASSWORD,
-  database: process.env.AWS_RDS_DATABASE,
-  multipleStatements: true,
-});
-// module.exports = connection;
-module.exports = pool;
+ var connection = mysql.createConnection({
+   host: process.env.AWS_RDS_ENDPOINT,
+   user: process.env.AWS_RDS_USERNAME,
+   password: process.env.AWS_RDS_PASSWORD,
+   port: process.env.AWS_RDS_PORT,
+   database: process.env.AWS_RDS_DATABASE,
+   multipleStatements: true,
+ });
+
+ module.exports = connection;
